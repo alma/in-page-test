57aa79902334a9233e5125840b9834d5407037a3
Nicolas Cordin <nicolas.cordin@getalma.eu>
2023-01-30 15:25:37 +0100

    fix: ci 14

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      skipDiagnostics: false,
+      logDiagnostics: false,
+      outputDir: "dist/types",
+    }),
+  ],
   build: {
     lib: {
       name: "Alma",
       entry: "src/index.ts",
       formats: ["es", "umd"],
-      fileName: (format) => `in-page.${format}.js`,
+      fileName: (format) => `index.${format}.js`,
     },
     outDir: "dist",
     rollupOptions: {
